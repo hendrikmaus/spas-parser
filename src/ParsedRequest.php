95c4c3b943d72e226d6816b605f644b22f0e2e18
cdamian <cosmin.damian@trivago.com>
2016-12-09 11:38:55 +0100

    Adding custom eror message to request.

--- src/ParsedRequest.php
+++ src/ParsedRequest.php
@@ -190,4 +190,15 @@ interface ParsedRequest
      * @return ParsedRequest
      */
     public function setRepetitionConfig(Repetition $config) : ParsedRequest;
+
+    /**
+     * @return string
+     */
+    public function getCustomErrorMessage() : string;
+
+    /**
+     * @param String $customErrorMessage
+     * @return ParsedRequest
+     */
+    public function setCustomErrorMessage(String $customErrorMessage) : ParsedRequest;
 }
